673bef8a2e7f9690048caa6c25df3efdad360a7f
zhengjunhao <changchunjunhao@gmail.com>
2018-10-13 15:42:07 +0800

    (refact) refact the code

---
+++ main.js
@@ -0,0 +1,63 @@
+
+console.log('include event.js');
+
+var canvasOnHit = function(){};
+function main() {
+    var c = canvas();
+    var canvasOnHit = c.canvasOnHit;
+    window.addEventListener('keydown', keyDownHandler, false);
+}
+
+function keyDownHandler(e) {
+    var key = e.keyCode ? e.keyCode :e.which;
+    switch(key) {
+    case 65:
+	playDo(); canvasOnHit(1); break;
+    case 83:
+	playRe(); canvasOnHit(2); break;
+    case 68:
+	playMi(); canvasOnHit(3); break;
+    case 70:
+	playFa(); canvasOnHit(4); break;
+    case 74:
+	playSo(); canvasOnHit(5); break;
+    case 75:
+	playLa(); canvasOnHit(6); break;
+    case 76:
+	playSi(); canvasOnHit(7); break;
+    case 186:
+	playDoh(); canvasOnHit(8); break;
+    }
+}
+
+var canvas = function() {
+
+    this.canvasOnHit = function (tone) {
+	// tone is 1 2 3 ... 7 8
+	selectedTemplate.onToneHit(tone);
+    };
+
+    var dict = {};
+    var t1 = template1();
+    dict['jumpingVol'] = new Template(t1.toneHit, t1.enable, t1.close);
+    dict['other'] = new Template(function(){}, function(){}, function(){});
+
+    var _s = document.getElementById('select');
+    var selectedTemplate = dict[_s.options[_s.selectedIndex].value];
+    selectedTemplate.enable();
+    _s.onchange = function() {
+	selectedTemplate.close();
+	selectedTemplate = dict[_s.options[_s.selectedIndex].value];
+	selectedTemplate.enable();
+    };
+
+    function Template(onToneHit, enable, close) {
+	this.onToneHit = onToneHit;
+	this.enable = enable;
+	this.close = close;
+    }
+
+    return this;
+};
+
+main();
--- template1.js
+++ template1.js
@@ -1,73 +1,143 @@
 
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+console.log('include template1.js')
 
-var Color = {
-blue: 'rgb(0,0,255)'
+var template1 = function() {
+    var dc = dataController();
+    var cc = canvasController(dc);
+
+    var requestId = null;
+    var iFading = null;
+
+    this.toneHit = function(tone) {
+	dc.template1OnHitAtTone(tone);
+    };
+    this.enable = function() {
+	requestId = window.requestAnimationFrame(cc.drawCanvas);
+	iFading = setInterval(dc.fading, 500);
+    };
+    this.close = function() {
+	window.cancelAnimationFrame(requestId);
+	clearInterval(iFading);
+    };
+    return this;
 };
 
-var templateConfig = {
-rWidth: 30,
-rHeightMultiple: 10,
-wGap: 10
-};
-
-var templatePosition = {
-startX: 10,
-startY: canvas.height - 10
-};
-
-function drawCanvas() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.fillStyle = Color.blue;
-
-    var x = templatePosition.startX;
-    var y = templatePosition.startY;
-    var rWidth = templateConfig.rWidth;
-    var rHeightM = templateConfig.rHeightMultiple;
-    var wGap = templateConfig.wGap;
-
-    var _md = getMusicData();
-    for (i = 0; i < _md.length; i++) {
-	var rHeight = (_md[i] + 1) * rHeightM;
-	ctx.fillRect(x, y - rHeight, rWidth, rHeight);
-	x += rWidth + wGap;
+var dataController = function() {
+    var musicData = [];
+    var lastNoiseTimes = [];
+    for (i = 0; i < 8; i++) {
+	musicData[i] = 0;
+    }
+    for (i = 0; i < musicData.length; i++) {
+        lastNoiseTimes[i] = new Date().getTime();
     }
 
-    ctx.closePath();
-    ctx.fill();
+    this.template1OnHitAtTone = function(tone) {
+	var index = tone - 1;
+        if (index < 0) {
+	    index = 0;
+        }
+        if (index > 7) {
+	    index = 7;
+        }
+        var maxToneDb = 20;
+        var toneIncrement = 3;
+        musicData[index] += toneIncrement;
+        if (musicData[index] > maxToneDb) {
+	    musicData[index] = maxToneDb;
+        }
+        lastNoiseTimes[index] = new Date().getTime();
+    }
 
-    window.requestAnimationFrame(drawCanvas);
-}
-
-// Add music buffer to smooth the increment and decrement.
-var musicBuffer = [];
-var templateTime = new Date().getTime();
-function getMusicData() {
-    var _md = musicData;
-    if (musicBuffer.length < _md.length) {
-	for (i = 0; i < _md.length; i++) {
-	    musicBuffer[i] = _md[i];
-	}
-    } else {
-	var delta = 1;
-	var thresholdTime = 50;
-	var deltaTime = new Date().getTime() - templateTime;
-	if (deltaTime > thresholdTime) {
-	    templateTime = new Date().getTime();  // lazy, but beauty
-	    for (i = 0; i < _md.length; i++) {
-		var gap = _md[i] - musicBuffer[i];
-		if (gap != 0) {
-		    musicBuffer[i] += (gap > 0 ? 1 : -1) * delta;
+    this.fading = function() {
+        var noiseReduceMultiple = 0.03;
+        for (i = 0; i < 8; i++) {
+	    if (musicData[i] > 2) {
+		var current = new Date().getTime();
+		var gap = current - lastNoiseTimes[i];
+		var change = Math.floor(gap * noiseReduceMultiple);
+		if (change >= musicData[i]) {
+		    change = musicData[i] - 1;
 		}
+		musicData[i] -= change;
+	    } else if (musicData[i] > 0) {
+		musicData[i] -= 1;
 	    }
 	}
     }
-    return musicBuffer;
-}
 
+    // Add music buffer to smooth the increment and decrement.
+    var musicBuffer = [];
+    var templateTime = new Date().getTime();
+    this.getMusicData = function() {
+	var _md = musicData;
+	if (musicBuffer.length < _md.length) {
+	    for (i = 0; i < _md.length; i++) {
+		musicBuffer[i] = _md[i];
+	    }
+        } else {
+	    var delta = 1;
+	    var thresholdTime = 50;
+	    var deltaTime = new Date().getTime() - templateTime;
+	    if (deltaTime > thresholdTime) {
+		templateTime = new Date().getTime();  // lazy, but beauty
+		for (i = 0; i < _md.length; i++) {
+		    var gap = _md[i] - musicBuffer[i];
+		    if (gap != 0) {
+			musicBuffer[i] += (gap > 0 ? 1 : -1) * delta;
+		    }
+		}
+	    }
+        }
+        return musicBuffer;
+    }
+    return this;
+};
 
+var canvasController = function(dc) {
 
-// kick
-window.requestAnimationFrame(drawCanvas);
+    var _dataController = dc;
+    var canvas = document.getElementById('canvas');
+
+    var Color = {
+    blue: 'rgb(0,0,255)'
+    };
+
+    var templateConfig = {
+    rWidth: 30,
+    rHeightMultiple: 10,
+    wGap: 10
+    };
+
+    var templatePosition = {
+    startX: 10,
+    startY: canvas.height - 10
+    };
+
+    this.drawCanvas = function() {
+
+        var ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+        ctx.fillStyle = Color.blue;
+
+        var x = templatePosition.startX;
+        var y = templatePosition.startY;
+        var rWidth = templateConfig.rWidth;
+        var rHeightM = templateConfig.rHeightMultiple;
+        var wGap = templateConfig.wGap;
+
+        var _md = _dataController.getMusicData();
+        for (i = 0; i < _md.length; i++) {
+	    var rHeight = (_md[i] + 1) * rHeightM;
+	    ctx.fillRect(x, y - rHeight, rWidth, rHeight);
+	    x += rWidth + wGap;
+        }
+
+        ctx.closePath();
+        ctx.fill();
+
+        requestId = window.requestAnimationFrame(drawCanvas);
+    }
+    return this;
+};

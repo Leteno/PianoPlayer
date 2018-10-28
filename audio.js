1c2802b49f73622de78d263e3c489ccff0c7be24
zhengjunhao <changchunjunhao@gmail.com>
2018-10-28 10:45:00 +0800

    支持同时播放音乐

--- audio.js
+++ audio.js
@@ -1,13 +1,8 @@
 
 console.log('include audio.js');
 
-var playingA = null;
 function play(tone) {
     var audioElement = document.getElementById(tone);
-    if (playingA != null) {
-	playingA.pause();
-	playingA.currentTime = 0;
-    }
+    audioElement.currentTime = 0;
     audioElement.play();
-    playingA = audioElement;
 }

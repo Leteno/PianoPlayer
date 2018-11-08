35d2132d7c1eb582547b0d7468ec93ed8f6873bb
zhengjunhao <changchunjunhao@gmail.com>
2018-11-08 23:04:05 +0800

    (refact) 重构 main.js

--- main.js
+++ main.js
@@ -1,92 +1,88 @@
 
 console.log('include event.js');
 
-var canvasOnHit = function(){};
-function main() {
-    var c = canvas();
-    var canvasOnHit = c.canvasOnHit;
-    window.addEventListener('keydown', keyDownHandler, false);
-}
-
-function keyDownHandler(e) {
-    var key = e.keyCode ? e.keyCode :e.which;
-    console.log('press at ' + key)
-    switch(key) {
-    case 90:
-	play('doL'); canvasOnHit(1); break;
-    case 88:
-	play('reL'); canvasOnHit(2); break;
-    case 67:
-	play('miL'); canvasOnHit(3); break;
-    case 86:
-	play('faL'); canvasOnHit(4); break;
-    case 77:
-	play('soL'); canvasOnHit(5); break;
-    case 188:
-	play('laL'); canvasOnHit(6); break;
-    case 190:
-	play('siL'); canvasOnHit(7); break;
-    case 65:
-	play('do'); canvasOnHit(1); break;
-    case 83:
-	play('re'); canvasOnHit(2); break;
-    case 68:
-	play('mi'); canvasOnHit(3); break;
-    case 70:
-	play('fa'); canvasOnHit(4); break;
-    case 74:
-	play('so'); canvasOnHit(5); break;
-    case 75:
-	play('la'); canvasOnHit(6); break;
-    case 76:
-	play('si'); canvasOnHit(7); break;
-    case 81:
-	play('doH'); canvasOnHit(8); break;
-    case 87:
-	play('reH'); canvasOnHit(2); break;
-    case 69:
-	play('miH'); canvasOnHit(3); break;
-    case 82:
-	play('faH'); canvasOnHit(4); break;
-    case 85:
-	play('soH'); canvasOnHit(5); break;
-    case 73:
-	play('laH'); canvasOnHit(6); break;
-    case 79:
-	play('siH'); canvasOnHit(7); break;
-    }
-}
-
-var canvas = function() {
-
-    this.canvasOnHit = function (tone) {
-	// tone is 1 2 3 ... 7 8
-	selectedTemplate.onToneHit(tone);
-    };
-
-    var dict = {};
-    var t1 = template1();
-    dict['jumpingVol'] = new Template(t1.toneHit, t1.enable, t1.close);
-    var t2 = template2();
-    dict['scrollingVol'] = new Template(t2.toneHit, t2.enable, t2.close);
-    dict['other'] = new Template(function(){}, function(){}, function(){});
-
-    var _s = document.getElementById('select');
-    var selectedTemplate = dict[_s.options[_s.selectedIndex].value];
-    selectedTemplate.enable();
-    _s.onchange = function() {
-	selectedTemplate.close();
-	selectedTemplate = dict[_s.options[_s.selectedIndex].value];
-	selectedTemplate.enable();
-    };
-
-    function Template(onToneHit, enable, close) {
-	this.onToneHit = onToneHit;
-	this.enable = enable;
-	this.close = close;
-    }
-
-    return this;
+var MAIN = function(){
+    this.selectedTemplate = null;
 };
 
-main();
+MAIN.prototype.run = function () {
+    this.init();
+    var that = this;
+    window.addEventListener('keydown', function(e) {that.handleKeyDown(e)}, false);
+};
+
+MAIN.prototype.handleKeyDown = function (e) {
+    var key = e.keyCode ? e.keyCode :e.which;
+    var datas = [
+        {'key': 90, 'music': 'doL', 'tone': 1 },
+        {'key': 88, 'music': 'reL', 'tone': 2 },
+        {'key': 67, 'music': 'miL', 'tone': 3 },
+        {'key': 86, 'music': 'faL', 'tone': 4 },
+        {'key': 77, 'music': 'soL', 'tone': 5 },
+        {'key': 188,'music': 'laL', 'tone': 6 },
+        {'key': 190,'music': 'siL', 'tone': 7 },
+        {'key': 65, 'music': 'do', 'tone': 1 },
+        {'key': 83, 'music': 're', 'tone': 2 },
+        {'key': 68, 'music': 'mi', 'tone': 3 },
+        {'key': 70, 'music': 'fa', 'tone': 4 },
+        {'key': 74, 'music': 'so', 'tone': 5 },
+        {'key': 75, 'music': 'la', 'tone': 6 },
+        {'key': 76, 'music': 'si', 'tone': 7 },
+        {'key': 81, 'music': 'doH', 'tone': 8 },
+        {'key': 87, 'music': 'reH', 'tone': 2 },
+        {'key': 69, 'music': 'miH', 'tone': 3 },
+        {'key': 82, 'music': 'faH', 'tone': 4 },
+        {'key': 85, 'music': 'soH', 'tone': 5 },
+        {'key': 73, 'music': 'laH', 'tone': 6 },
+        {'key': 79, 'music': 'siH', 'tone': 7 }
+    ];
+    var i;
+    for (i = 0; i < datas.length; i++) {
+        d = datas[i];
+        if (d['key'] === key) {
+	    play(d['music']);
+	    this.notifyCanvas(d['tone']);
+	    break;
+        }
+    }
+};
+
+MAIN.prototype.init = function() {
+
+    function template (onToneHit, enable, close) {
+	return {
+	    "onToneHit": onToneHit,
+	    "enable": enable,
+	    "close": close
+	}
+    }
+
+    var dict = {};
+    var t1 = new template1();
+    dict['jumpingVol'] = template(t1.toneHit, t1.enable, t1.close);
+    var t2 = new template2();
+    dict['scrollingVol'] = template(t2.toneHit, t2.enable, t2.close);
+    dict['other'] = template(function(){}, function(){}, function(){});
+
+    var _s = document.getElementById('select');
+    var selectedValue = _s.options[_s.selectedIndex].value
+
+    var that = this;
+    that.selectedTemplate = dict[selectedValue];
+    that.selectedTemplate.enable();
+    _s.onchange = function() {
+	that.selectedTemplate.close();
+	var selectedValue = _s.options[_s.selectedIndex].value;
+	that.selectedTemplate = dict[selectedValue];
+	that.selectedTemplate.enable();
+    };
+};
+
+MAIN.prototype.notifyCanvas = function(tone) {
+    if (this.selectedTemplate != null) {
+	this.selectedTemplate.onToneHit(tone);
+    }
+};
+
+var m = new MAIN();
+m.run();
